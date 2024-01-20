c68b75c180480c3d0236e19162afffc2152e8c4c
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2024-01-19 23:24:38 -0600

    basic rewrite

--- static/scripts/language.js
+++ static/scripts/language.js
@@ -1,5 +1,13 @@
 const startBtn = document.querySelector(".start-btn");
 const articleInput = document.querySelector(".article-input");
+const richtextCont = document.querySelector(".richtext-cont");
+
+// TODO: modular
+const foreignWordSpan = document.querySelector(".foreign-word");
+const passageTranslate = document.querySelector(".passage-translate");
+const translatedWordSpan = document.querySelector(".translated-word");
+
+let passage;
 
 let languageModel;
 
@@ -7,4 +15,13 @@ startBtn.addEventListener("click", () => {
     // create handler
     const text = articleInput.value;
     languageModel = new Spanish(text);
+
+    richtextCont.textContent = "";
+
+    const lines = text.split("\n").map(line => line.trim());
+
+    for (const line of lines) {
+        const lineEl = languageModel.createLineEl(line);
+        richtextCont.append(lineEl);
+    }
 });
--- static/scripts/languageModels/languageModel.js
+++ static/scripts/languageModels/languageModel.js
@@ -1,40 +1,107 @@
 class LanguageModel {
     /**
-     * Creates a language model handler
-     * @param {string} text article
+     * @param {string} text article text
      * @param {string} lang language code
-     */
+    */
     constructor(text, lang) {
+        // TODO: decide if this.text is necessary
         this.text = text;
         this.lang = lang;
     }
 
-    /**
-     * @abstract
-     * @returns {string[]}
-     */
-    _getWords() { }
+    _getWords(_line) { }
+
+    _createWordElement(_word, _i, _arr) { }
 
     /**
-     * @abstract
-     * @param {string} _word word
-     * @param {number} _i index
-     * @returns {HTMLSpanElement}
+     * @param {string} line
      */
-    _createWordElement(_word, _i) { }
+    createLineEl(line) {
+        const out = document.createElement("p");
 
-    createLine() {
+        // if line empty, return an empty paragraph
+        if (!line) return out;
 
-        wordEl.addEventListener("click", async () => {
+        const words = this._getWords(line);
+
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+            const wordEl = this._createWordElement(word, i, words);
+
+            wordEl.addEventListener("click", async () => {
+                // TODO: modular
+                passageTranslate.classList.add("grayed-out");
+                const res = await this.translate(word);
+                passageTranslate.classList.remove("grayed-out");
+
+                if (passage) passage.style.removeProperty("background");
+                passage = wordEl;
+
+                foreignWordSpan.textContent = word;
+                translatedWordSpan.textContent = res;
+            });
+
+            out.append(wordEl);
+        }
+
+        // TODO: modular
+        const translateBtn = document.createElement("button");
+        translateBtn.textContent = "translate";
+
+        translateBtn.addEventListener("click", async () => {
             passageTranslate.classList.add("grayed-out");
-            const res = await translate(word);
+            const res = await this.translate(line);
             passageTranslate.classList.remove("grayed-out");
 
             if (passage) passage.style.removeProperty("background");
-            passage = wordEl;
+            passage = out;
 
-            foreignWordSpan.textContent = word;
-            translatedWordSpan.textContent = res.translatedText;
+            foreignWordSpan.textContent = "<sentence>";
+            translatedWordSpan.textContent = res;
         });
+
+        const speakBtn = document.createElement("button");
+        speakBtn.textContent = "speak";
+
+        speakBtn.addEventListener("click", async () => {
+            // https://stackoverflow.com/questions/64662877/how-to-add-a-pause-and-stop-function-to-my-javascript-text-to-speech
+
+            const speaker = new SpeechSynthesisUtterance(line);
+            const voiceName = "Microsoft Xiaoxiao Online (Natural) - Chinese (Mainland)";
+            // speaker.voice = voices[voiceName];
+
+            for (let i = 0; i < voices.length; i++) {
+                if (voices[i].name === voiceName) {
+                    speaker.voice = voices[i];
+                    break;
+                }
+            }
+
+            window.speechSynthesis.speak(speaker);
+        });
+
+        out.append(translateBtn, speakBtn);
+
+        return out;
+    }
+
+    /**
+     * @param {string} text
+     * @returns {Promise<string>}
+     */
+    async translate(text) {
+        const res = await fetch(TRANSLATE_URL + "/translate", {
+            method: "POST",
+            body: JSON.stringify({
+                q: text,
+                source: this.lang,
+                target: "en",
+                format: "text",
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        return (await res.json()).translatedText;
     }
 }
