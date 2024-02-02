589fc6f3ef7954d986464709b24efe0bd783d7cb
Junhao Zhang <jerry_mountain@hotmail.com>
2024-02-02 11:09:37 -0600

    pretty much finished flashcards

--- static/kana/main.js
+++ static/kana/main.js
@@ -1,6 +1,8 @@
 const hiraganaChars = "あかさたないきしちにうくすつぬえけせてねおこそとのんはまやらひみりふむゆるへめれほもよろわがざだばぱぎじぢびぴぐずづぶぷげぜでべぺをごぞどぼぽ".split("");
 const katakanaChars = "アカサタナイキシチニウクスツヌエケセテネオコソトノンハマヤラヒミリフムユルヘメレホモヨロワガザダバパギジヂビピグズヅブプゲゼデベペヲゴゾドボポ".split("");
 const romajiArr = "a ka sa ta na i ki shi chi ni u ku su tsu nu e ke se te ne o ko so to no n ha ma ya ra hi mi ri fu mu yu ru he me re ho mo yo ro wa ga za da ba pa gi ji ji bi pi gu zu zu bu pu ge ze de be pe wo go zo do bo po".split(" ");
+// const romajiArr = "a ka sa ta".split(" ");
+
 
 /** [romaji] -> [hiragana] */
 const hiraganaObj = {};
@@ -32,8 +34,11 @@ const backText = document.querySelector(".back .text");
 const cardFront = document.querySelector(".front");
 const cardBack = document.querySelector(".back");
 
-let cardIndex = 0;
+shuffleArray(romajiArr);
+
+let cardIndex = -1;
 let savedArray = [];
+let isReviewMode = false;
 
 flipBtn.addEventListener("click", () => {
     cardBack.classList.remove("hide")
@@ -41,16 +46,62 @@ flipBtn.addEventListener("click", () => {
 });
 
 saveBtn.addEventListener("click", () => {
-    // TODO: add to saved array
+    if (isReviewMode) {
+        savedArray.push(savedArray[cardIndex]);
+    } else {
+        savedArray.push(cardIndex);
+    }
     newKana();
-})
+});
 
 nextBtn.addEventListener("click", newKana);
 
 newKana();
 
+document.addEventListener("keydown", e => {
+    if (!cardFront.classList.contains("hide")) {
+        // front of the card
+        if (e.key == " ") {
+            flipBtn.click();
+        }
+    } else {
+        // back of card
+        if (e.key == "2") {
+            saveBtn.click();
+        } else if (e.key == "1") {
+            nextBtn.click();
+        }
+    }
+});
+
 function newKana() {
-    const romaji = romajiArr[cardIndex];
+    cardIndex++;
+
+    let index;
+    if (!isReviewMode && cardIndex == romajiArr.length) {
+        if (savedArray.length > 0) {
+            isReviewMode = true;
+            cardIndex = 0;
+            cardFront.classList.add("review");
+            cardBack.classList.add("review");
+        } else {
+            console.log("done!");
+            return;
+        }
+    } else if (isReviewMode && cardIndex == savedArray.length) {
+        console.log("done!");
+        return;
+    }
+
+    if (isReviewMode) {
+        index = savedArray[cardIndex];
+        cardPrompt.textContent = `Review: ${cardIndex + 1} / ${savedArray.length}`;
+    } else {
+        index = cardIndex;
+        cardPrompt.textContent = `${cardIndex + 1} / ${romajiArr.length}`
+    }
+
+    const romaji = romajiArr[index];
     const hiragana = hiraganaObj[romaji];
 
     frontText.textContent = hiragana;
@@ -58,6 +109,4 @@ function newKana() {
 
     cardBack.classList.add("hide");
     cardFront.classList.remove("hide");
-
-    cardIndex++;
 }
